2f5e6b5aa0f57c1ae25daceabaf53ba17c620742
ptaeierdam <ptaeierdam@gmail.com>
2022-02-15 19:38:04 -0700

    Initial commit

---
+++ src/main/java/com/csc205/project2/Shape.java
@@ -0,0 +1,5 @@
+package com.csc205.project2;
+
+public abstract class Shape implements ThreeDimensionalShape {
+
+}
---
+++ src/main/java/com/csc205/project2/ThreeDimensionalShape.java
@@ -0,0 +1,9 @@
+package com.csc205.project2;
+
+public interface ThreeDimensionalShape {
+
+    double surfaceArea();
+
+    double volume();
+
+}
